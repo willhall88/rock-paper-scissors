286e3795b4d4296a7256905645e030e967d3ef78
Will Hall <willhall88@hotmail.com>
2014-05-12 14:29:10 +0100

    refactored

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -9,7 +9,6 @@ Player.prototype.picks = function(pick) {
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
-
 };
 
 var PAIRS = {
@@ -20,14 +19,18 @@ var PAIRS = {
 
 
 Game.prototype.winner = function() {
-    if(this.player1.pick === this.player2.pick){
+    if(this.same_pick()){
       return null;
     }
-    if(PAIRS[this.player1.pick]['beats'] === this.player2.pick){
+    else if(PAIRS[this.player1.pick]['beats'] === this.player2.pick){
       return this.player1;
     }
     else {
       return this.player2;
-    }
-    
+    }   
 };
+
+Game.prototype.same_pick = function(){
+  return this.player1.pick === this.player2.pick;
+};
+  
